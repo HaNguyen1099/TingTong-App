6b19e15b65da76ac26fc7c618beb98e23214bb0b
dcthoai <dthoai2k3@gmail.com>
2023-11-13 23:23:42 +0700

    fix error when checked in postRoomStep2

--- app/src/main/java/com/example/tingtongapp/Views/PostRoomStep2.java
+++ app/src/main/java/com/example/tingtongapp/Views/PostRoomStep2.java
@@ -3,7 +3,9 @@ package com.example.tingtongapp.Views;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -48,8 +50,8 @@ public class PostRoomStep2 extends Fragment implements View.OnClickListener{
         edtNumberPeoplePushRoom =view.findViewById(R.id.edt_number_people_push_room);
         edtLengthPushRoom =view.findViewById(R.id.edt_length_push_room);
         edtWidthPushRoom =view.findViewById(R.id.edt_width_push_room);
-
         edtPriceRoomPushRoom =view.findViewById(R.id.edt_priceRoom_push_room);
+
         edtElectricBillPushRoom =view.findViewById(R.id.edt_electricBill_push_room);
         edtWaterBillPushRoom =view.findViewById(R.id.edt_waterBill_push_room);
         edtInternetPushRoom =view.findViewById(R.id.edt_internet_push_room);
@@ -63,13 +65,105 @@ public class PostRoomStep2 extends Fragment implements View.OnClickListener{
         btnNextStep2PostRoom = view.findViewById(R.id.btn_nextStep2_post_room);
         btnNextStep2PostRoom.setOnClickListener(this);
 
+        edtElectricBillPushRoom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+                // Before data is change
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // When data is change
+                String newText = charSequence.toString();
+
+                if (!newText.isEmpty() && !newText.equals("0")) {
+                    chBoxFreeElectricPushRoom.setChecked(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // After data is change
+            }
+        });
+
+        edtWaterBillPushRoom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+                // Before data is change
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // When data is change
+                String newText = charSequence.toString();
+
+                if (!newText.isEmpty() && !newText.equals("0")) {
+                    chBoxFreeWaterPushRoom.setChecked(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // After data is change
+            }
+        });
+
+        edtInternetPushRoom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+                // Before data is change
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // When data is change
+                String newText = charSequence.toString();
+
+                if (!newText.isEmpty() && !newText.equals("0")) {
+                    chBoxFreeInternetPushRoom.setChecked(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // After data is change
+            }
+        });
+
+        edtParkingPushRoom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+                // Before data is change
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // When data is change
+                String newText = charSequence.toString();
+
+                if (!newText.isEmpty() && !newText.equals("0")) {
+                    chBoxFreeParkingPushRoom.setChecked(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // After data is change
+            }
+        });
+
         chBoxFreeElectricPushRoom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
                     edtElectricBillPushRoom.setText("0");
                 }else{
-                    edtElectricBillPushRoom.setText("");
+                    String textOn = edtElectricBillPushRoom.getText().toString();
+
+                    if(textOn.equals("0")) {
+                        edtElectricBillPushRoom.setText("");
+                    }
                 }
             }
         });
@@ -79,7 +173,11 @@ public class PostRoomStep2 extends Fragment implements View.OnClickListener{
                 if(isChecked){
                     edtWaterBillPushRoom.setText("0");
                 }else{
-                    edtWaterBillPushRoom.setText("");
+                    String textOn = edtWaterBillPushRoom.getText().toString();
+
+                    if(textOn.equals("0")) {
+                        edtWaterBillPushRoom.setText("");
+                    }
                 }
             }
         });
@@ -90,7 +188,11 @@ public class PostRoomStep2 extends Fragment implements View.OnClickListener{
                 if(isChecked){
                     edtInternetPushRoom.setText("0");
                 }else{
-                    edtInternetPushRoom.setText("");
+                    String textOn = edtInternetPushRoom.getText().toString();
+
+                    if(textOn.equals("0")) {
+                        edtInternetPushRoom.setText("");
+                    }
                 }
             }
         });
@@ -101,7 +203,11 @@ public class PostRoomStep2 extends Fragment implements View.OnClickListener{
                 if(isChecked){
                     edtParkingPushRoom.setText("0");
                 }else{
-                    edtParkingPushRoom.setText("");
+                    String textOn = edtParkingPushRoom.getText().toString();
+
+                    if(textOn.equals("0")) {
+                        edtParkingPushRoom.setText("");
+                    }
                 }
             }
         });
@@ -137,16 +243,16 @@ public class PostRoomStep2 extends Fragment implements View.OnClickListener{
             ){
                 Toast.makeText(getContext(), "Vui lòng điền đầy đủ thông tin", Toast.LENGTH_SHORT).show();
             } else {
-                if(TextUtils.isEmpty(electricityPriceString) || !chBoxFreeElectricPushRoom.isChecked()){
+                if(TextUtils.isEmpty(electricityPriceString) && !chBoxFreeElectricPushRoom.isChecked()){
                     checkData = false;
                 }
-                if(TextUtils.isEmpty(waterPriceString) || !chBoxFreeWaterPushRoom.isChecked()){
+                if(TextUtils.isEmpty(waterPriceString) && !chBoxFreeWaterPushRoom.isChecked()){
                     checkData = false;
                 }
-                if(TextUtils.isEmpty(internetPriceString) || !chBoxFreeInternetPushRoom.isChecked()){
+                if(TextUtils.isEmpty(internetPriceString) && !chBoxFreeInternetPushRoom.isChecked()){
                     checkData = false;
                 }
-                if(TextUtils.isEmpty(parkingFeeString) || !chBoxFreeParkingPushRoom.isChecked()){
+                if(TextUtils.isEmpty(parkingFeeString) && !chBoxFreeParkingPushRoom.isChecked()){
                     checkData = false;
                 }
 
