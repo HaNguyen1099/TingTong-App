72a825e0365851645bbb46d33f64438a0766910e
Ha Nguyen Ngoc <98647965+HaNguyen1099@users.noreply.github.com>
2023-11-24 15:55:43 +0700

    done

--- app/src/main/java/com/example/tingtongapp/Views/SearchView.java
+++ app/src/main/java/com/example/tingtongapp/Views/SearchView.java
@@ -18,9 +18,7 @@ import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
-import com.example.tingtongapp.Adapters.AdapterRecyclerFilter;
 import com.example.tingtongapp.Adapters.AdapterRecyclerSuggestions;
 import com.example.tingtongapp.Adapters.AdapterRoomSuggestion;
 import com.example.tingtongapp.ClassOther.myFilter;
@@ -36,16 +34,13 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 public class SearchView extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, ICallBackSearchView {
-
     public static final String REQUEST = "requestcode";
     public final static int REQUEST_DISTRICT = 99;
-
     // Save state of 4 fragment instead reload
     private HashMap<Integer, Fragment> fragmentHashMap = new HashMap<>();
     ImageView ivBack;
-    RecyclerView recyclerFilter,recyclerSearchRoom;
+    RecyclerView recyclerSearchRoom;
     EditText edTSearch;
     ProgressBar progessBarLoad;
     LinearLayout lnLtResultReturnSearchView;
@@ -55,7 +50,6 @@ public class SearchView extends AppCompatActivity implements View.OnClickListene
     FrameLayout fragmentContainer;
     String district;
     List<myFilter> filterList;
-    AdapterRecyclerFilter adapterRecyclerFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,20 +59,17 @@ public class SearchView extends AppCompatActivity implements View.OnClickListene
         initControl();
         getDistrict();
     }
-
     @Override
     protected void onStart() {
         super.onStart();
         callSearchRoomController();
         getDistrict();
     }
-
     private void getDistrict(){
         Intent intent = getIntent();
         district = intent.getStringExtra(AdapterRecyclerSuggestions.INTENT_DISTRICT);
         //Set text cho district
         edTSearch.setText(district);
-
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("ListRoom");
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -96,7 +87,6 @@ public class SearchView extends AppCompatActivity implements View.OnClickListene
                         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(SearchView.this);
                         recyclerSearchRoom.setLayoutManager(linearLayoutManager);
                         recyclerSearchRoom.setAdapter(adapterRoomSuggestion);
-
                         progessBarLoad.setVisibility(View.GONE);
                         lnLtResultReturnSearchView.setVisibility(View.VISIBLE);
                         txtNumberRoom.setText(listRoom.size() + "");
@@ -105,18 +95,14 @@ public class SearchView extends AppCompatActivity implements View.OnClickListene
                     }
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
     }
-
     private void initData(){
         filterList = new ArrayList<myFilter>();
     }
-
     private void initControl(){
         edTSearch = findViewById(R.id.edT_search);
         edTSearch.setOnClickListener(new View.OnClickListener() {
@@ -127,34 +113,21 @@ public class SearchView extends AppCompatActivity implements View.OnClickListene
                 startActivityForResult(intent,REQUEST_DISTRICT);
             }
         });
-
         txtNumberRoom = findViewById(R.id.txt_number_room);
-
         progessBarLoad = findViewById(R.id.progress_bar_search_view);
         // Edit color for progressbar
         progessBarLoad.getIndeterminateDrawable().setColorFilter(Color.parseColor("#F54500"),
                 android.graphics.PorterDuff.Mode.MULTIPLY);
         // Hide on the first load
         progessBarLoad.setVisibility(View.GONE);
-
         lnLtResultReturnSearchView = (LinearLayout) findViewById(R.id.lnLt_resultReturn_search_view);
-
         nestedScrollSearchView = (NestedScrollView) findViewById(R.id.nested_scroll_search_view);
         progressBarLoadMoreSearchView = (ProgressBar) findViewById(R.id.progress_bar_load_more_search_view);
         progressBarLoadMoreSearchView.getIndeterminateDrawable().setColorFilter(Color.parseColor("#F54500"),
                 android.graphics.PorterDuff.Mode.MULTIPLY);
 
-
         fragmentContainer = findViewById(R.id.fragment_container);
-
-        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.HORIZONTAL);
-        recyclerFilter.setLayoutManager(staggeredGridLayoutManager);
-
-        adapterRecyclerFilter = new AdapterRecyclerFilter(this,R.layout.search_filter_element_recyclerview, filterList);
-        recyclerFilter.setAdapter(adapterRecyclerFilter);
-
         recyclerSearchRoom = findViewById(R.id.recycler_search_room);
-
 
         ivBack = findViewById(R.id.iv_back);
         ivBack.setOnClickListener(new View.OnClickListener() {
@@ -164,32 +137,21 @@ public class SearchView extends AppCompatActivity implements View.OnClickListene
             }
         });
     }
-
     @Override
     public void onClick(View view) {
-
     }
-
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-
     }
-
     @Override
     public void addFilter(myFilter filter) {
-
     }
-
     @Override
     public void replaceFilter(myFilter filter) {
-
     }
-
     @Override
     public void removeFilter(myFilter filter) {
-
     }
-
     private void callSearchRoomController(){
         // Display progress bar
         progessBarLoad.setVisibility(View.VISIBLE);
